dbdd98d204b80efd68e359ab7e0a0ff4827d4a33
Vishnupatel <vcvanthala.5546@gmail.com>
2019-01-17 21:28:53 +0530

    130 Redirecting and Wildcard Routes

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -14,7 +14,10 @@ export class ServerComponent implements OnInit {
   constructor(private serversService: ServersService,
               private route: ActivatedRoute,
               private router: Router) { }
-
+// queryParams
+// fragment
+// queryParamsHandling
+// +params['id']
   ngOnInit() {
     const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(id);
